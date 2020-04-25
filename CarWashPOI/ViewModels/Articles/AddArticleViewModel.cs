726916f64ca8debd12998d8606e12950513c2391
vzehirev <velizar.zehirev@gmail.com>
2020-04-25 13:29:34 +0300

    Small fixes

--- CarWashPOI/ViewModels/Articles/AddArticleViewModel.cs
+++ CarWashPOI/ViewModels/Articles/AddArticleViewModel.cs
@@ -6,9 +6,11 @@ namespace CarWashPOI.ViewModels.Articles
     public class AddArticleViewModel
     {
         [Required(ErrorMessage = "Полето Заглавие е задължително.")]
+        [MinLength(4, ErrorMessage = "Полето Заглавие трябва да е с дължина от минимум 4 символа")]
         public string Title { get; set; }
 
         [Required(ErrorMessage = "Статията трябва да има съдържание.")]
+        [MinLength(11, ErrorMessage = "Полето Съдържание трябва да е с дължина от минимум 11 символа")]
         public string Content { get; set; }
 
         public string UserId { get; set; }
